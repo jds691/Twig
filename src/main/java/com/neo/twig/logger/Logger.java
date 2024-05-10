55a7dfe6f0769550c72fd091d2a57fbf11f6132a
Neo <52429194+jds691@users.noreply.github.com>
2024-05-10 23:50:42 +0100

    Make logger use runtime name (AppConfig.name) if available

--- src/main/java/com/neo/twig/logger/Logger.java
+++ src/main/java/com/neo/twig/logger/Logger.java
@@ -1,9 +1,12 @@
 package com.neo.twig.logger;
 
+import com.neo.twig.Engine;
+
 @SuppressWarnings("unused")
 public class Logger {
     private static Level loggingLevel = Level.INFO;
     private String name;
+    private String spaceName;
     private boolean isEngineClass;
 
     private Logger() {
@@ -14,7 +17,13 @@ public class Logger {
         Logger logger = new Logger();
 
         logger.name = type.getSimpleName();
-        logger.isEngineClass = type.getPackageName().startsWith("com.neo.twig");
+        if (type.getPackageName().startsWith("com.neo.twig")) {
+            logger.spaceName = "Twig";
+        } else if (!Engine.getConfig().appConfig().name.isEmpty()) {
+            logger.spaceName = Engine.getConfig().appConfig().name;
+        } else {
+            logger.spaceName = "Unnamed Runtime";
+        }
 
         return logger;
     }
@@ -25,32 +34,32 @@ public class Logger {
 
     public void logVerbose(String message) {
         if (loggingLevel.ordinal() < Level.INFO.ordinal()) {
-            System.out.printf("[%s - %s - Verbose]: %s\n", isEngineClass ? "Twig" : "Runtime", name, message);
+            System.out.printf("[%s - %s - Verbose]: %s\n", spaceName, name, message);
         }
     }
 
     public void logInfo(String message) {
         if (loggingLevel.ordinal() < Level.DEBUG.ordinal())
-            System.out.printf("[%s - %s - Info]: %s\n", isEngineClass ? "Twig" : "Runtime", name, message);
+            System.out.printf("[%s - %s - Info]: %s\n", spaceName, name, message);
     }
 
     public void logDebug(String message) {
         if (loggingLevel.ordinal() < Level.WARNING.ordinal())
-            System.out.printf("[%s - %s - Debug]: %s\n", isEngineClass ? "Twig" : "Runtime", name, message);
+            System.out.printf("[%s - %s - Debug]: %s\n", spaceName, name, message);
     }
 
     public void logWarning(String message) {
         if (loggingLevel.ordinal() < Level.ERROR.ordinal())
-            System.out.printf("[%s - %s - Warning]: %s\n", isEngineClass ? "Twig" : "Runtime", name, message);
+            System.out.printf("[%s - %s - Warning]: %s\n", spaceName, name, message);
     }
 
     public void logError(String message) {
         if (loggingLevel.ordinal() < Level.FATAL.ordinal())
-            System.out.printf("[%s - %s - Error]: %s\n", isEngineClass ? "Twig" : "Runtime", name, message);
+            System.out.printf("[%s - %s - Error]: %s\n", spaceName, name, message);
     }
 
     public void logFatal(String message) {
-        System.out.printf("[%s - %s - Fatal]: %s\n", isEngineClass ? "Twig" : "Runtime", name, message);
+        System.out.printf("[%s - %s - Fatal]: %s\n", spaceName, name, message);
     }
 
     public enum Level {
