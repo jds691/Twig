88fa195e4d3a6cef3fb8ef796d79ed6de799225c
Neo <52429194+jds691@users.noreply.github.com>
2024-05-13 04:11:55 +0100

    Add verbose logging to ResourceService

--- src/main/java/com/neo/twig/resources/ResourceService.java
+++ src/main/java/com/neo/twig/resources/ResourceService.java
@@ -1,10 +1,14 @@
 package com.neo.twig.resources;
 
+import com.neo.twig.logger.Logger;
+
 import java.util.HashMap;
 
 public class ResourceService {
     private HashMap<Integer, Object> resourceCache = new HashMap<>();
     private HashMap<Integer, Integer> referenceCount = new HashMap<>();
+
+    private final Logger logger = Logger.getFor(ResourceService.class);
 
     public <T> T getResource(int hashCode) {
         Object cachedResource = resourceCache.get(hashCode);
@@ -13,6 +17,7 @@ public class ResourceService {
             int references = referenceCount.get(hashCode);
             references++;
             referenceCount.put(hashCode, references);
+            logger.logVerbose(String.format("Hit resource cache for '%s', current ref count: %d", hashCode, references));
             return (T) cachedResource;
         }
 
@@ -29,6 +34,7 @@ public class ResourceService {
         references--;
 
         if (references <= 0) {
+            logger.logVerbose(String.format("Resource '%s' no longer referenced. Releasing...", hashCode));
             referenceCount.remove(hashCode);
             resourceCache.remove(hashCode);
         }
